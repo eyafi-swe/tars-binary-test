f06ce781d77482c0593db322425e2192031daaa2
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-12-21 11:03:54 +0600

    Final touch

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -29,27 +29,27 @@ const Header = () => {
 
         <div className='navbarContainer h-16'>
 
-            <nav className='lg:w-11/12   mx-auto flex justify-between items-center'>
+            <nav className='lg:w-11/12 md:w-full   mx-auto flex justify-between items-center'>
 
-                <div className='w-11/12 lg:w-max lg:mx-0  mx-auto flex justify-between items-center'>
+                <div className='w-11/12 md:w-max md:mx-0  mx-auto flex justify-between items-center'>
 
-                    <div className="h-6 w-6 text-white lg:hidden mr-2" onClick={() => setOpen(!open)}>
+                    <div className="h-6 w-6 text-white md:hidden mr-2" onClick={() => setOpen(!open)}>
                         {
                             open ? <XMarkIcon /> : <Bars2Icon />
                         }
                     </div>
 
-                    <img src={logo} alt="Logo" className=' h-16' />
+                    <img src={logo} alt="Logo" className='tarsLogo h-16' />
 
-                    <div className='lg:hidden '>
+                    <div className='md:hidden '>
                         <img src={shoppingbag} alt="Shopping Bag Icon" className='h-7' />
                     </div>
 
                 </div>
 
-                <div className={`navItems  text-white text-sm   lg:flex lg:w-max lg:py-0 w-screen py-4   absolute lg:static duration-700 ease-in ${open ? 'top-16' : 'top-[-1200px]'}`}>
+                <div className={`navItems  text-white text-sm   md:flex md:w-max md:py-0 w-screen py-4   absolute md:static md:duration-75 duration-700 ease-in ${open ? 'top-16' : 'top-[-1200px]'}`}>
 
-                    <div className='lg:hidden flex justify-center w-full border-b border-gray-500 py-2 px-2'>
+                    <div className='md:hidden flex justify-center w-full border-b border-gray-500 py-2 px-2'>
 
                         <div className='border-gray-500 border w-full flex items-center rounded-lg px-2'>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
@@ -63,13 +63,13 @@ const Header = () => {
 
                 </div>
 
-                <div className='lg:flex  hidden'>
+                <div className='md:flex  hidden'>
                     <div className=''>
-                        <img src={search} alt="Search Icon" className='h-7 text-gray-300' />
+                        <img src={search} alt="Search Icon" className='h-7 text-gray-300 searchIcon' />
                     </div>
 
                     <div className='ml-2'>
-                        <img src={shoppingbag} alt="Shopping Bag Icon" className='h-7' />
+                        <img src={shoppingbag} alt="Shopping Bag Icon" className='h-7 bagIcon' />
                     </div>
                 </div>
 
--- src/Components/Link/Link.js
+++ src/Components/Link/Link.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Link = ({rout}) => {
     return (
-        <div className='mx-2 lg:border-none lg:py-0 lg:mb-0 border-b mb-1 py-2 border-gray-500 '>
+        <div className='mx-2 md:border-none md:py-0 md:mb-0 border-b mb-1 py-2 border-gray-500 '>
             <a href= {rout.path} className=' text-gray-300'>{rout.name}</a>
         </div>
     );
